a09c16bc77396cceb84454a2ad516d99bfb643f7
ramjoy8 <ramjoy1996@gmail.com>
2019-05-04 18:22:08 +0530

    updates to deploy to heroku

--- route.js
+++ route.js
@@ -34,26 +34,26 @@ exec(function(err, result) {
 
 
 
-router.post('/event/saveevent',(req,res,next)=>{
-  let newEvent = new Event({
+// router.post('/event/saveevent',(req,res,next)=>{
+//   let newEvent = new Event({
    
-   name: req.body.name,
-   date: req.body.name,
-   time: req.body.time,
-   price: req.body.price,
-   imageUrl: req.body.imageUrl,
-   location: req.body.location,
-
+//    name: req.body.name,
+//    date: req.body.name,
+//    time: req.body.time,
+//    price: req.body.price,
+//    imageUrl: req.body.imageUrl,
+//    location: req.body.location,
+//    sessions:req.body.sessions
    
 
 
 
-   })
-   newEvent.save((err,event)=>{
+//    })
+//    newEvent.save((err,event)=>{
 
-    
 
-  });
+
+//   });
 
 
 
